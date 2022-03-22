8a114e6b5340cbe14be0c4f26b7672a8179f59b9
Lux <92374048+lucas38460@users.noreply.github.com>
2022-03-22 13:50:42 +0100

    creation des hobbies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
+let hobbie = [
+    {id: 0, name: "vélo"},
+    {id: 1, name: "moto"},
+    {id: 2, name: "couture"},
+    {id: 3, name: "écrire des histoires"},
+    {id: 5, name: "programmer"},
+    {id: 6, name: "faire du cheval"},
+    {id: 7, name: "dessiner"},
+    {id: 8, name: "chanter"},
+    {id: 4, name: "sortir dans la nature"},
+    {id: 9, name: "faire du sport"},
+    {id: 10, name: "écouter de la musique"},
+    {id: 11, name: ""},
+    {id: 12, name: ""},
+    {id: 13, name: ""},
+    {id: 14, name: ""},
+    {id: 15, name: ""},
+    {id: 16, name: ""},
+    {id: 17, name: ""},
+    {id: 18, name: ""},
+    {id: 19, name: ""},
+    
+];
+
+
+
 import { v4 as uuidv4 } from 'uuid';
 uuidv4();
+
 for (let i = 0; i < 1000; i++) {
     console.log(uuidv4());
 }
